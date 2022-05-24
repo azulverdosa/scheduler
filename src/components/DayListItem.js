027c38e065ecfc201f26b5a94a54d3802c6a8fe3
AvaCado <91709114+azulverdosa@users.noreply.github.com>
2022-05-24 10:28:20 -0400

    imported files and edited component

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,16 +1,31 @@
 import React from 'react';
+import cx from 'classnames';
+import './DayListItems.scss';
+
+const formatSpots = (spots) => {
+  switch (true) {
+    case spots === 0:
+      return 'no spots';
+
+    case spots === 1:
+      return '1 spot';
+
+    default:
+      return `${spots} spots`;
+  }
+};
 
 export default function DayListItem({ name, selected, setDay, spots }) {
   const clickHandler = (event) => setDay(name);
+  const dayClass = cx('day-list__item', {
+    'day-list__item--selected': selected,
+    'day-list__item--full': spots === 0,
+  });
 
   return (
-    <li onClick={clickHandler} className={name}>
+    <li onClick={clickHandler} className={dayClass}>
       <h2 className="text--regular">{name}</h2>
-      <h3 className="text--light">{spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots(spots)} remaining</h3>
     </li>
   );
 }
-
-// The <li> represents the entire day item
-// The <h2> should display the day name
-// The <h3> should display the spots remaining for a day
