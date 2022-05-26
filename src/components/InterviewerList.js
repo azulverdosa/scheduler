eb42c09574ad52558fcee481c4808d50df029ba7
AvaCado <91709114+azulverdosa@users.noreply.github.com>
2022-05-26 14:18:17 -0400

    Updated InterviewerList component

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,11 +1,28 @@
 import React from 'react';
+
+import InterviewerListItem from './InterviewerListItem';
 import './InterviewerList.scss';
 
-const InterviewerList = () => {
+const InterviewerList = (props) => {
+  const interviewers = props.interviewers.map((interviewer) => {
+    const clickHandler = (event) => props.onChange(interviewer.id);
+    return (
+      <InterviewerListItem
+        key={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        selected={interviewer.id === props.selectedInterviewerId}
+        setInterviewer={clickHandler}
+      />
+    );
+  });
+
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list"></ul>
+      <ul className="interviewers__list">{interviewers}</ul>
     </section>
   );
 };
+
+export default InterviewerList;
