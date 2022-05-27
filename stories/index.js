57276ed68e2d7216780ded0cfba096b6e3da2887
AvaCado <91709114+azulverdosa@users.noreply.github.com>
2022-05-27 11:02:23 -0400

    import Form.jsx to index.js & formated Form.jsx

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Show from 'components/Appointment/Show';
 import Confirm from 'components/Appointment/Confirm';
 import Status from 'components/Appointment/Status';
 import Error from 'components/Appointment/Error';
+import Form from 'components/Appointment/Form';
 
 storiesOf('Button', module)
   .addParameters({
@@ -149,4 +150,16 @@ storiesOf('Appointment', module)
   .add('Status', () => <Status message={'Deleting'} />)
   .add('Error', () => (
     <Error message={'Could not delete appointment'} onClose={action('onClose')} />
+  ))
+  .add('Edit', () => (
+    <Form
+      student="Ava"
+      interviewer={4}
+      interviewers={interviewers}
+      onSave={action('onSave')}
+      onCancel={action('onCancel')}
+    />
+  ))
+  .add('Create', () => (
+    <Form interviewers={[]} onSave={action('onSave')} onCancel={action('onCancel')} />
   ));
