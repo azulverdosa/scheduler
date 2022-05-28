1a362327d6c58b3e6480c5e4f9e080c2e4846328
AvaCado <91709114+azulverdosa@users.noreply.github.com>
2022-05-28 18:51:00 -0400

    import files and update Appointment component

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 
+import Header from './Header';
+import Empty from './Empty';
+import Show from './Show';
 import './styles.scss';
 
 const Appointment = (props) => {
-  return <article className="appointment"></article>;
+  return (
+    <article className="appointment">
+      <Header time={props.time} />
+
+      {props.interview?.student && props.interview?.interviewer ? (
+        <Show student={props.interview.student} interviewer={props.interview.interviewer} />
+      ) : (
+        <Empty />
+      )}
+    </article>
+  );
 };
 
 export default Appointment;
