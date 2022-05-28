6b6145e0fdac13b9a5161cdf1a652a54b4de2f7e
AvaCado <91709114+azulverdosa@users.noreply.github.com>
2022-05-28 18:50:02 -0400

    added conditional

--- src/components/Appointment/Show.jsx
+++ src/components/Appointment/Show.jsx
@@ -6,7 +6,7 @@ const Show = ({ student, interviewer, onEdit, onDelete }) => {
       <section className="appointment__card-left">
         <h2 className="text--regular">{student}</h2>
         <section className="interviewer">
-          <h4 className="text--light">{interviewer.name}</h4>
+          <h4 className="text--light">{interviewer?.name}</h4>
           <h3 className="text--regular">Sylvia Palmer</h3>
         </section>
       </section>
