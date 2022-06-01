e4349b518dfb779e0375a48cda85bc932f811e7b
AvaCado <91709114+azulverdosa@users.noreply.github.com>
2022-06-01 19:45:19 -0400

    update mode for EMPTY, SHOW & CREATE

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -4,16 +4,32 @@ import Header from './Header';
 import Empty from './Empty';
 import Show from './Show';
 import './styles.scss';
+import useVisualMode from 'hooks/useVisualMode';
+import Form from './Form';
 
-const Appointment = (props) => {
+const EMPTY = 'EMPTY';
+const SHOW = 'SHOW';
+const CREATE = 'CREATE';
+
+const Appointment = (props, interviewers) => {
+  const { transition, back, mode } = useVisualMode(props.interview ? SHOW : EMPTY);
+
   return (
     <article className="appointment">
       <Header time={props.time} />
 
-      {props.interview?.student && props.interview?.interviewer ? (
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
         <Show student={props.interview.student} interviewer={props.interview.interviewer} />
-      ) : (
-        <Empty />
+      )}
+      {mode === CREATE && (
+        <Form
+          // student={props.interview.student}
+          // interviewer={props.interview.interviewer}
+          interviewers={interviewers}
+          // onSave={() => {}}
+          onCancel={() => back(EMPTY)}
+        />
       )}
     </article>
   );
