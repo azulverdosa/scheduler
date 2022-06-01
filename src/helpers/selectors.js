92909545156a71bf8d9fea63b43735b1f35a6648
AvaCado <91709114+azulverdosa@users.noreply.github.com>
2022-06-01 19:46:00 -0400

    create getInterviewersForDay

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -6,6 +6,14 @@ export const getAppointmentsForDay = (state, selectedDay) => {
     : [];
 };
 
+export const getInterviewersForDay = (state, selectedDay) => {
+  const found = state?.days?.find((day) => selectedDay === day.name);
+
+  return found
+    ? found?.interviewers?.map((interviewerId) => state.interviewers[interviewerId])
+    : [];
+};
+
 export const getInterview = (state, interview) => {
   const interviewerID = interview?.interviewer;
   const interviewerData = state.interviewers[interviewerID];
